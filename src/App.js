f17bd68a481ede5c05b04e7e6b0197e35fb49eb5
Nobaliz <jonathancasstellano12@gmail.com>
2024-11-16 20:03:33 -0500

    Avance en la integracion del bot, y manejo de la base de datos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './style.css';
+require('dotenv').config();
 //import ChatbotIA from './ChatbotIA'; // Importa el componente
 import ChatbotComponent from './ChatbotComponent';
+
 
 export default function App() {
   return (
